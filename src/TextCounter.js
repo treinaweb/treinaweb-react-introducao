c17004ec299b816ad035bb2dcf9cec3bddb65777
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-14 15:11:45 -0200

    aula-05-03

--- src/TextCounter.js
+++ src/TextCounter.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 
 
 class TextCounter extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            totalChars: 0
+        }
+    }
+
     render(){
+        const {state} = this;
         return (
             <div>
                 <h1>Meu Contador</h1>
                 <textarea />
                 <div>
-                    <strong>Total:</strong>
+                    <strong>Total:</strong> { state.totalChars }
                 </div>
             </div>
         )
