440aa57f7f206a36f6b5d80dfaedd46f3a6950b1
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-14 14:57:56 -0200

    aula-05-02

---
+++ src/TextCounter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class TextCounter extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Meu Contador</h1>
+                <textarea />
+                <div>
+                    <strong>Total:</strong>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TextCounter;
