3fa142e353abf1bd87483877fb1781aa498ca0d9
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-17 08:13:34 -0200

    aula-06-02

--- src/MyForm.js
+++ src/MyForm.js
@@ -5,7 +5,8 @@ class MyForm extends Component{
         super(props);
 
         this.state = {
-            name: 'abc'
+            name: '',
+            message: ''
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -24,6 +25,10 @@ class MyForm extends Component{
                 <label>
                     Name:
                     <input type="text" name="name" value={state.name} onChange={this.handleChange} /> {state.name}
+                </label>
+                <label>
+                    Message:
+                    <textarea value={state.message} />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>
