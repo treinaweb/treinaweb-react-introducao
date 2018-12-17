4c81ef91a8e17dfd6d085ae4a2dba02bee5faa20
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-17 10:33:30 -0200

    aula-07-01

--- src/MyForm2.js
+++ src/MyForm2.js
@@ -8,11 +8,21 @@ class MyForm2 extends Component{
         this.inputName = React.createRef();
 
         this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            list: []
+        }
     }
 
     handleSubmit(event){
-        console.log(this.inputName.current.value);
         event.preventDefault();
+        fetch(`https://api.github.com/search/repositories?q=${this.inputName.current.value}`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    list: data.items
+                })
+            })
     }
 
 
@@ -23,10 +33,14 @@ class MyForm2 extends Component{
                 <div>
                     <label>
                         Name:
-                        <input defaultValue={'abcde'} type="text" name="name" ref={this.inputName} />
+                        <input type="text" name="name" ref={this.inputName} />
                     </label>
                 </div>
                 <input type="submit" value="Enviar" />
+
+                <ul>
+                    {state.list.map(item => <li>{item.full_name}</li>)}
+                </ul>
             </form>
         )
     }
